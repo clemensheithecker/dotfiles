236b5b7f6267a170f8668d8ee4c53f86072d833e
Clemens S. Heithecker <clemens@heithecker.dev>
2021-02-24 23:12:42 +0100

    Fix install.sh to recognize files not starting with a dot

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ alias gitdf="git --git-dir=$DOTFILES --work-tree=$HOME"
 # Update the working tree and check for a potential error of inability
 # to overwrite pre-existing dotfiles
 if gitdf checkout; then
-    echo "Checked out dotfiles."
+	echo "Checked out dotfiles."
 else
-    echo "Backing up pre-existing dotfiles..."
-    mkdir -p $BACKUP
-    gitdf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BACKUP/{}
-    gitdf checkout
-    echo "Checked out dotfiles."
+	echo "Backing up pre-existing dotfiles..."
+	mkdir -p $BACKUP
+	gitdf checkout 2>&1 | egrep "\t+" | awk {'print $1'} | xargs -I{} mv {} $BACKUP/{}
+	gitdf checkout
+	echo "Checked out dotfiles."
 fi
 
 # Disable showing untracked files for gitdf status
